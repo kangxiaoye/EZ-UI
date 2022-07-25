fc7f2f69701953888f07b55d3b84f9296f8a8f23
kangxiaoye <2198695788@qq.com>
2022-07-25 13:56:08 +0800

    添加row 和col 基本实现 row 和 col 的功能

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
+
 
 new Vue({
     el: '#app',
@@ -15,6 +21,20 @@ new Vue({
         loading1: false,
         loading2: true,
         loading3: false,
+        message: 'value:双向绑定'
+    },
+    created(){
+        setTimeout(()=>{
+            let event = new Event('change');
+            let inputElement = this.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+
+        },3000)
+    },
+    methods: {
+        inputChange (e) {
+            console.log(e)
+        }
     }
 })
 import chai from 'chai'
